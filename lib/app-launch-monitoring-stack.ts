26598c33e37d8cc6252c110527fac7dc6de39ff0
Jeffrey LEE <jeffreyleeon@gmail.com>
2023-11-19 11:46:14 +0800

    Initial commit

---
+++ lib/app-launch-monitoring-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import { aws_s3 as s3 } from 'aws-cdk-lib';
+
+export class AppLaunchMonitoringStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Policy?
+
+    // Lambda
+    
+
+    // API Gateway
+
+    // output - url
+
+  }
+}
---
+++ test/macrodroid-aws-monitoring.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MacrodroidAwsMonitoring from '../lib/macrodroid-aws-monitoring-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/macrodroid-aws-monitoring-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MacrodroidAwsMonitoring.MacrodroidAwsMonitoringStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
